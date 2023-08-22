e025fd58509dbf60718e6d00aa43d0268ba9be95
satara <25549072+satara2358@users.noreply.github.com>
2023-08-22 01:33:49 -0500

    Calendar

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ import Form from "./scenes/form";
 // import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-// import Calendar from "./scenes/calendar/calendar";
+import Calendar from "./scenes/calendar";
+
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -34,11 +35,11 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
-              <Route path="/calendar" element={<Calendar />} />
               <Route path="/geography" element={<Geography />} />*/}
             </Routes> 
           </main>
