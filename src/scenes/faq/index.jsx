dec820afa67a881e3034b656b51279595948f9f9
satara <25549072+satara2358@users.noreply.github.com>
2023-08-22 21:54:36 -0500

    FQA

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import { Box, useTheme, Typography } from '@mui/material'
+import Header from '../../components/Header'
+import AccordionSummary  from '@mui/material/AccordionSummary'
+import Accordion from '@mui/material/Accordion'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { tokens } from '../../theme'
 
-type Props = {}
+const FAQ = () => {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
 
-const index = (props: Props) => {
+
   return (
-    <div>index</div>
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Preguntas y respuestas frecuentes" />
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+          <Typography color={colors.greenAccent[500]} variant='h5'>
+            ¿Qué es el sistema de gestión de turnos?
+          </Typography>
+ 
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ducimus molestiae officiis officia beatae accusantium harum, ipsam dolore obcaecati debitis, sunt id sequi, nihil quae? Excepturi explicabo delectus maiores aut.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+          <Typography color={colors.greenAccent[500]} variant='h5'>
+            ¿Qué es el sistema de gestión de turnos?
+          </Typography>
+ 
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ducimus molestiae officiis officia beatae accusantium harum, ipsam dolore obcaecati debitis, sunt id sequi, nihil quae? Excepturi explicabo delectus maiores aut.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+          <Typography color={colors.greenAccent[500]} variant='h5'>
+            ¿Qué es el sistema de gestión de turnos?
+          </Typography>
+ 
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ducimus molestiae officiis officia beatae accusantium harum, ipsam dolore obcaecati debitis, sunt id sequi, nihil quae? Excepturi explicabo delectus maiores aut.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+          <Typography color={colors.greenAccent[500]} variant='h5'>
+            ¿Qué es el sistema de gestión de turnos?
+          </Typography>
+ 
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ducimus molestiae officiis officia beatae accusantium harum, ipsam dolore obcaecati debitis, sunt id sequi, nihil quae? Excepturi explicabo delectus maiores aut.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Box>
+
+      </Box>
+    </Box>
   )
 }
+
+export default FAQ
